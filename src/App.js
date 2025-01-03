3b9720b9096c651962520f484ab805b45c94fa2c
Anirudh8756 <anirudh@madhulata.com>
2025-01-03 16:31:42 +0530

    project save

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Provider } from 'react-redux';
 import appstore from './redux/Store';
 import Zuzapp from './Zuzapp';
 import './css/root.scss';
+import ReactDOM from 'react-dom'
+
+
 function App() {
   return (
     <Provider store={appstore}>
--- src/pages/Inbox.js
+++ src/pages/Inbox.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import  { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMessage } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+// Alternatively, for brands or other icon libraries:
+// import { faMessage } from '@fortawesome/free-brands-svg-icons';
+
 const Inbox = () => {
   return (
     <div className={`inbox rel`}>
-      Inbox / Side bar 
-      </div>
+      <div className={`userbox flex`}>
+        <div className={`you rel flex`}>
+        <Link className={`user`}>
+          <img src={`https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg`}/>
+        </Link>
+        </div>
+        <div className={`actions rel flex aic`}>
+          <Link>
+          <FontAwesomeIcon icon={faMessage} />
+          </Link>
+          <Link>
+          <FontAwesomeIcon icon={faEllipsisVertical} />
+          </Link>
+        </div>
+        </div>
+    </div>
   )
 }
 
